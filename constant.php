a2be3ce3ca9c07acb54f024480c831e2fddeae79
ayurahmaayu <46545678+ayurahmaayu@users.noreply.github.com>
2019-01-10 10:10:28 +0700

    oop ayurahmawati

---
+++ constant.php
@@ -0,0 +1,33 @@
+<?php 
+
+// define( 'NAMA', 'Ayu Rahmawati');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 16;
+// echo UMUR;
+
+// class Coba {
+// 	const NAMA = 'Ayu Rahmawati';
+// }
+
+// echo Coba::NAMA;
+
+
+
+// echo __FILE__;
+
+
+// function coba() {
+// 	return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
