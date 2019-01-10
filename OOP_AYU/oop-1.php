fea84f209e88b840004bad3b21b85e8f3dc64921
ayurahmaayu <46545678+ayurahmaayu@users.noreply.github.com>
2019-01-10 10:25:53 +0700

    oopAyu

---
+++ OOP_AYU/oop-1.php
@@ -0,0 +1,19 @@
+<?php 
+
+Class Orang {
+	public $nama = 'Andi';
+	public $jenisKelamin;
+
+	public function makan()
+	{
+		return $this->nama.' sedang makan <br>';
+	}
+} 
+
+$saya = new Orang;
+$saya->nama = 'Noni';
+echo$saya->makan();
+
+$saya->nama = 'Ane';
+echo $saya->makan();
+?>
